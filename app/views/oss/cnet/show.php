39e99c50a74703eea8585e04dad1bf6051b6a12c
zxj95121 <986773071@qq.com>
2017-05-27 18:50:49 +0800

    hehe

--- app/views/oss/cnet/show.php
+++ app/views/oss/cnet/show.php
@@ -161,18 +161,30 @@ $signPackage = $jssdk->GetSignPackage();
 		    // wx.hideAllNonBaseMenuItem();
 
 		    wx.onMenuShareTimeline({
-		    title: '<?php echo $nickname; ?>'+'在使用微信新功能管理校园网，快来看看吧。', // 分享标题
-		    link: 'wechat.zhangxianjian.com/oss/cnet/cnet', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		    imgUrl: 'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=333f3ac494510fb367197097e932c893/a8014c086e061d95df89434571f40ad163d9ca84.jpg', // 分享图标
-		    success: function () { 
-		        // 用户确认分享后执行的回调函数
-		        alert('恭喜你，分享成功');
-		    },
-		    cancel: function () { 
-		        // 用户取消分享后执行的回调函数
-		        alert('你竟然取消分享。。');
-		    }
-		});
+			    title: '<?php echo $nickname; ?>'+'在使用微信新功能管理校园网，快来看看吧。', // 分享标题
+			    link: 'wechat.zhangxianjian.com/oss/cnet/cnet', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: 'http://img06.tooopen.com/images/20170304/tooopen_sl_200486667368.jpg', // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			        alert('恭喜你，分享成功');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			        alert('你竟然取消分享。。');
+			    }
+			});
+			wx.onMenuShareQZone({
+			    '<?php echo $nickname; ?>'+'在使用微信新功能管理校园网，快来看看吧。', // 分享标题
+			    desc: '记得在微信打开链接哦', // 分享描述
+			    link: 'wechat.zhangxianjian.com/oss/cnet/cnet', // 分享链接
+			    imgUrl: 'http://img06.tooopen.com/images/20170304/tooopen_sl_200486667368.jpg', // 分享图标
+			    success: function () { 
+			       // 用户确认分享后执行的回调函数
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+			});
 		});
 	</script>
 
